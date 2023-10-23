bbe17cdaf1aa763f1fa3ed51eb9508365438afa4
Matias Rivas <mat500.600@gmail.com>
2023-10-23 12:55:25 -0300

    agregados cambios a app.js necesarios para validar el formulario

--- src/app.js
+++ src/app.js
@@ -4,9 +4,23 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { validate } from "webpack";
+const card = document.getElementById("Card#");
+const CVC = document.getElementById("CVC#");
+const amount = document.getElementById("amount");
+const firstname = document.getElementById("FirstName#");
+const lastname = document.getElementById("LastName#");
+const city = document.getElementById("city");
+const inputstate = document.getElementById("inputState");
+const postalcode = document.getElementById("postalcode");
+const paymentmethod = document.getElementById("paymentmethod");
+const message = document.getElementById("message");
 
 const Sendbutton = document.getElementById("Sendbutton");
 
-Sendbutton.addEventListener("click", function(e) {
+form.addEventListener("submit", function(e) {
   e.preventDefault();
+  validateInputs();
 });
+
+const validateInputs = () => {};

8b715e82432a6c13d5861935b7a7c537cbc2e0ee
Matias Rivas <mat500.600@gmail.com>
2023-10-23 12:17:13 -0300

    generado esqueleto, pendiente funcionalidad boton, validacion

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const Sendbutton = document.getElementById("Sendbutton");
+
+Sendbutton.addEventListener("click", function(e) {
+  e.preventDefault();
+});
